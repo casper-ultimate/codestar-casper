e17a6e54b8858c6203030d2af3333a16e0185f94
powercreek <landanparker@gmail.com>
2023-01-11 06:51:18 +0000

    fixed test.js

--- tests/test.js
+++ tests/test.js
@@ -5,10 +5,10 @@ var index = require('../index.js');
 
 describe('Tests index', function() {
   it('verifies successful response', function(done) {
-    index.get({ /* event */ }, { /* context */ }, (err, result) => {
+    index.get2({ /* event */ }, { /* context */ }, (err, result) => {
       try {
         test.number(result.statusCode).is(200);
-        test.string(result.body).contains('world');
+        test.string(result.body).contains('World');
         test.value(result).hasHeader('content-type', 'text/html');
         done();
       } catch(error) {
